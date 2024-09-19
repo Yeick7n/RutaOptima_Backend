2ea154fbcc764f99c13df17fea9c0dab00d80530
yeick7n <stickcardozo03@gmail.com>
2024-09-19 14:20:46 -0500

    Actualización controller cargue-datos

--- src/cargue-archivo/cargue-archivo.controller.ts
+++ src/cargue-archivo/cargue-archivo.controller.ts
@@ -1,5 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { CargueArchivoService } from './cargue-archivo.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('cargue-archivo')
-export class CargueArchivoController {}
+export class CargueArchivoController {
+
+    constructor(
+        private readonly cargueDatosService: CargueArchivoService
+    ){}
+
+    @Post('json')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadJson(@UploadedFile() file: Express.Multer.File): Promise<string> {
+        return await this.cargueDatosService.processJson(file);
+    }
+}
